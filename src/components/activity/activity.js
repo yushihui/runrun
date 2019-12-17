428daf64327c9b6021b2b621a58f5b5999414fb5
shihui <shihui.yu@netbraintech.com>
2019-12-17 16:42:28 -0500

    display activities

--- src/components/activity/activity.js
+++ src/components/activity/activity.js
@@ -1,39 +1,71 @@
+import React, {Component} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import classNames from "classnames";
+import './activity.css'
 
-import React, { Component } from 'react';
 
-class Activity extends Component {
+const activityStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+
+}));
+const MINUTES_PER_MILE = 26.8224;
+
+export default class Activity extends Component {
+
     state = {
         activities: []
-    }
+    };
+    ACTIVITY_WALK = "Walk";
+    ACTIVITY_RUN = "Run";
 
-    activityUrl='https://raw.githubusercontent.com/yushihui/go.strava/master/result/36533/activities.json'
+
+    activityUrl = 'https://raw.githubusercontent.com/yushihui/go.strava/master/result/36533/activities.json';
+
     componentDidMount() {
         fetch(this.activityUrl)
             .then(res => res.json())
             .then((data) => {
-                this.setState({ activities: data })
+                this.setState({activities: data})
             })
             .catch(console.log)
     }
-    render() {
-        return (
-            <div className="container">
-                <div className="col-xs-12">
-                    <h1>My Activities</h1>
-                    {this.state.activities.map((activity) => (
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">{activity.name}</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">
-                                    {activity.distance}
-                                </h6>
 
-                            </div>
-                        </div>
+    render() {
+
+        return (
+            <div className="">
+                <h3>My Activities</h3>
+                <Grid container spacing={3}>
+                    {this.state.activities.map((activity) => (
+                        <Grid item xs={12} lg={2} sm={6}>
+                            <Paper className={classNames("paper", {
+                                walk: activity.type === this.ACTIVITY_WALK
+                            })}>
+                                <span className="card-name">{activity.name}</span>
+                                <span className="card-time">{activity.start_date_local.substr(0, 10)}</span>
+                                <section>
+                                    <span
+                                        className="distance">D: {(activity.distance / 1609).toFixed(2)}<small>mi</small></span>
+                                    <span
+                                        className="distance">P: {activity.average_speed > 0 ? ((MINUTES_PER_MILE / activity.average_speed).toFixed(2)):activity.average_speed}<small>m/mi</small></span>
+                                    <span
+                                        className="distance">H: {activity.average_heartrate}</span>
+
+                                </section>
+                            </Paper>
+                        </Grid>
                     ))}
-                </div>
+                </Grid>
             </div>
         );
     }
 }
-export default Activity
