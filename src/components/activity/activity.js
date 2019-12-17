2cb622a8d17abae70bd4228b4dfa6130f481c6be
shihui <yushihui@gmail.com>
2019-12-17 11:39:05 -0500

    show activites

--- src/components/activity/activity.js
+++ src/components/activity/activity.js
@@ -1,14 +1,39 @@
-import React from 'react'
 
-const  Activity= () => (
-    <div>
-        <h2>Activity</h2>
-        <ul>
-            <li>6/5 @ Evergreens</li>
-            <li>6/8 vs Kickers</li>
-            <li>6/14 @ United</li>
-        </ul>
-    </div>
-)
+import React, { Component } from 'react';
 
+class Activity extends Component {
+    state = {
+        activities: []
+    }
+
+    activityUrl='https://raw.githubusercontent.com/yushihui/go.strava/master/result/36533/activities.json'
+    componentDidMount() {
+        fetch(this.activityUrl)
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ activities: data })
+            })
+            .catch(console.log)
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="col-xs-12">
+                    <h1>My Activities</h1>
+                    {this.state.activities.map((activity) => (
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">{activity.name}</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">
+                                    {activity.distance}
+                                </h6>
+
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
 export default Activity
